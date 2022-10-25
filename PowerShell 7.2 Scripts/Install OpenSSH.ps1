bbd2c8dcda1259b05c41ac8fc0c2da2e76c06456
Gustav <gustavhlarsen01@gmail.com>
2022-10-25 15:05:39 +0200

    Create Install OpenSSH.ps1

---
+++ PowerShell 7.2 Scripts/Install OpenSSH.ps1
@@ -0,0 +1,32 @@
+# Gustav Hagsten Larsen
+# Install SSH script
+
+
+# Enter SSH type.
+[int]$Type = Read-Host -Prompt "(1) Client | (2) Server | (3) Client and Server" -ForegroundColor blue
+
+switch ($Type) {
+    1 { Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0 }
+    2 { Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 }
+    3 { Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+        Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 }
+}
+
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+
+Get-Service -Name *ssh*
