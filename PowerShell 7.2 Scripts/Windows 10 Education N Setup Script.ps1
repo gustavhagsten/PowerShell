1e4352ff5169d33027cf4c85a4a64e164f89af0c
Gustav <gustavhlarsen01@gmail.com>
2022-10-25 14:27:14 +0200

    Update Windows 10 Education N Setup Script.ps1

--- PowerShell 7.2 Scripts/Windows 10 Education N Setup Script.ps1
+++ PowerShell 7.2 Scripts/Windows 10 Education N Setup Script.ps1
@@ -3,6 +3,8 @@
 
 
 # Functions
+# ===================================================================
+
 # Remove pinned programs.
 function UnpinTaskbar {
     param (
@@ -15,6 +17,7 @@ function UnpinTaskbar {
                 ForEach-Object{$_.DoIt(); $exec = $true} 
 }
 
+# ====================================================================
 
 # Create Shortcut
 function MakeDesktopShortcut {
@@ -36,23 +39,44 @@ function MakeDesktopShortcut {
     # Make shortcut to open as Admin
     if ($Admin) {
     $bytes = [System.IO.File]::ReadAllBytes("$HOME\Desktop\$Name.lnk")
-    $bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+    # Set byte 21 (0x15) bit 6 (0x20) ON
+    $bytes[0x15] = $bytes[0x15] -bor 0x20 
     [System.IO.File]::WriteAllBytes("$HOME\Desktop\$Name.lnk", $bytes)
     }
 }
 
+# ====================================================================
 
-# Remove default Taskbar pin.
-UnpinTaskbar -appname "Microsoft Store"
-UnpinTaskbar -appname "Mail"
+Function TaskbarDebloat {
+# Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarSmallIcons -Value 1
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowCortanaButton -Value 0
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowTaskViewButton -Value 0
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarAnimations -Value 0
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search' -Name SearchboxTaskbarMode -Value 0
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds' -Name ShellFeedsTaskbarViewMode -Value 2
+New-Item -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\' -Name Explorer 
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer' -Name DisableNotificationCenter -Value 1 -PropertyType DWord
+}
 
+# ====================================================================
 
-# Make Desktop Shortcut.
-MakeDesktopShortcut -SourceExe ncpa.cpl -Name "Network Connection" -IconNumber 164
-MakeDesktopShortcut -SourceExe sysdm.cpl -Name "System Properties" -IconNumber 272
-MakeDesktopShortcut -SourceExe "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Name "PowerShell Admin" -Admin
+function CreateProfile {
+    # Make Powershell profile.
+    if (test-path $PROFILE) {
+        Write-Output "Powershell Profile exist already."
+    }
+    else {
+        New-Item -Path $profile -Type File -Force
+    }
+}
 
+# ====================================================================
 
+function NetworkConfig {
+# Set static IP or use DHCP
+}
+
+# ====================================================================
 
 <# !! Not working because permission denied by WMI
 
@@ -65,29 +89,24 @@ MakeDesktopShortcut -SourceExe "C:\Windows\System32\WindowsPowerShell\v1.0\power
     ForEach-Object{$_.DoIt()}
 #>
 
+# ====================================================================
 
-# Edit Taskbar.
-# Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarSmallIcons -Value 1
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowCortanaButton -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowTaskViewButton -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarAnimations -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search' -Name SearchboxTaskbarMode -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds' -Name ShellFeedsTaskbarViewMode -Value 2
-New-Item -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\' -Name Explorer 
-New-ItemProperty -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer' -Name DisableNotificationCenter -Value 1 -PropertyType DWord
+# Remove default Taskbar pinned programs.
+UnpinTaskbar -appname "Microsoft Store"
+UnpinTaskbar -appname "Mail"
 
+# Make Desktop Shortcuts.
+MakeDesktopShortcut -SourceExe ncpa.cpl -Name "Network Connection" -IconNumber 164
+MakeDesktopShortcut -SourceExe sysdm.cpl -Name "System Properties" -IconNumber 272
+MakeDesktopShortcut -SourceExe "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Name "PowerShell Admin" -Admin
+
+TaskbarDebloat
+
+CreateProfile
 
 # Set Danish keyboard.
 Set-WinUserLanguageList -LanguageList en-DK -Force
 
 
-# Make Powershell profile.
-if (test-path $PROFILE) {
-    Write-Output "Powershell Profile exist already."
-}
-else {
-    New-Item -Path $profile -Type File -Force
-}
-
-
+# End script by Restarting the Computer.
 Restart-Computer
