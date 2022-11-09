bcd62d5a69b6999efa7ecb54c7f10094c41cbf36
gustav_hagsten <gustavhlarsen01@gmail.com>
2022-11-09 12:30:08 +0100

    Update

---
+++ Server/CreateADDSUser/CreateADDSUser.ps1
@@ -0,0 +1,9 @@
+# Import Logic
+. $PSScriptRoot\Logic.ps1
+
+
+
+
+
+# Start GUI
+. $PSScriptRoot\Form.ps1
--- Server/CreateADDSUser/Logic.ps1
+++ Server/CreateADDSUser/Logic.ps1
@@ -1,6 +1,6 @@
-$Username = "Gustav Hagsten Larsen"
-$GivenName = "Gustav"
-$Surname = "Hagsten Larsen"
-$LoginName = "gust"
+function FunctionName {
+    param ( $Username, $GivenName, $Surname, $LoginName )
 
-New-ADUser -Name $Username -GivenName $GivenName -Surname $Surname -SamAccountName $LoginName -Path "OU=DomainUsers,DC=DomainTest,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+    New-ADUser -Name $Username -GivenName $GivenName -Surname $Surname -SamAccountName $LoginName -Path "OU=DomainUsers,DC=DomainTest,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+    
+}
